4159fff8614fa1445faa7f83b266a4d18a7dfa5b
Rae Farine <rfarine1027@gmail.com>
2020-07-31 14:12:21 -0400

    lol

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Layout from 'components/layout';
 import Box from 'components/box';
