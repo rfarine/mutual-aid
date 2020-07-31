8a75c968132fa5db8539d55bc399cc2d0132ce1a
Rae Farine <rfarine1027@gmail.com>
2020-07-31 14:54:52 -0400

    Attempt to return json

--- api/date.go
+++ api/date.go
@@ -1,12 +1,19 @@
 package handler
 
 import (
-    "fmt"
-    "net/http"
-    "time"
+  "fmt"
+  "net/http"
+  "time"
+  "encoding/json"
 )
 
+type Response struct {
+  Date string `json:"date"`
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-    currentTime := time.Now().Format(time.RFC850)
-    fmt.Fprintf(w, currentTime)
+  currentTime := time.Now().Format(time.RFC850)
+  fmt.Fprintf(w, currentTime)
+  date := Response{Date: currentTime}
+  json.NewEncoder(w).Encode(date)
 }
