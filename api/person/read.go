539b353c2a29de3cfa8679a863e1b450a906ce6b
Rae Farine <rfarine1027@gmail.com>
2020-08-29 17:49:56 -0400

    Fix api/person/read.go

--- api/person/read.go
+++ api/person/read.go
@@ -2,21 +2,21 @@ package handler
 
 import (
   "net/http"
-  "encoding/json"
+  "os"
   f "gopkg.in/fauna/faunadb-go.v2/faunadb"
 )
 
 type Person struct {
-  ID string `json:"id"`
-  FirstName string `json:"firstName"`
-  LastName string `json:"lastName"`
-  Org string `json:"org"`
+  ID string `fauna:"id"`
+  FirstName string `fauna:"firstName"`
+  LastName string `fauna:"lastName"`
+  Org string `fauna:"org"`
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
   apiKey := os.Getenv("FAUNADB_SECRET")
   client := f.NewFaunaClient(apiKey)
-  id := request.PathParameters["id"]
+  id := r.PathParameters["id"]
 
   res, err := client.Query(f.Get(f.RefClass(f.Collection("person"), id)))
 
@@ -32,5 +32,5 @@ func Handler(w http.ResponseWriter, r *http.Request) {
   }
 
   w.Header().Set("Content-Type", "application/json")
-  w.Write(res)
+  w.Write(person)
 }
